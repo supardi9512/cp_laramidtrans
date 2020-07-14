e6b53c231852e2dd903863f14009e9a4d1379ac9
supardi9512 <supardi9512@gmail.com>
2020-07-14 21:02:07 +0700

    make model and migration donation

---
+++ app/Donation.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    // Ijinkan semua kolom dapat di inputkan
+    protected $guarded = [];
+
+    /**
+     * Set status to Pending
+     *
+     * @return void
+     */
+    public function setStatusPending()
+    {
+        $this->attributes['status'] = 'pending';
+        self::save();
+    }
+
+    /**
+     * Set status to Success
+     *
+     * @return void
+     */
+    public function setStatusSuccess()
+    {
+        $this->attributes['status'] = 'success';
+        self::save();
+    }
+
+    /**
+     * Set status to Failed
+     *
+     * @return void
+     */
+    public function setStatusFailed()
+    {
+        $this->attributes['status'] = 'failed';
+        self::save();
+    }
+
+    /**
+     * Set status to Expired
+     *
+     * @return void
+     */
+    public function setStatusExpired()
+    {
+        $this->attributes['status'] = 'expired';
+        self::save();
+    }
+}
